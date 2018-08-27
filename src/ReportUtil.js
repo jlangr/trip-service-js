73ea6fd48d295a16ea065d5ef91b856d980962a2
Jeff Langr <Jeff@Langrsoft.com>
2018-08-27 11:19:30 -0600

    report util init

---
+++ src/ReportUtil.js
@@ -0,0 +1,39 @@
+//x = string
+// op = StringOp enum
+const transform = (x, count, spacing, op) {
+  let buffer = '';
+  let buffer1 = '';
+  const w = count + spacing;
+  let pads;
+  switch (op) {
+      case 'under':
+          {
+              for (let i = 0; i < w - spacing; i++) {
+                  buffer += '-';
+              }
+              const ptext = buffer(); // need to be a copy?
+              pads = "";
+              buffer1 += ptext;
+              let l = w - ptext.length();
+              for (let j = 0; j < l; j++) {
+                  pads += " ";
+              }
+              buffer1 << pads;
+          }
+          break;
+      case 'pad':
+          {
+              pads = " ";
+              let l = spacing;
+              while (l > 1) {
+                  pads = pads + " ";
+                  l = l - 1;
+              }
+              buffer += x;
+              buffer += pads;
+              buffer1 += buffer;
+          }
+          break;
+  }
+  return buffer1;
+}
